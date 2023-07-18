ed5199069940ec04c9c54d476029424e108cb4ec
allan <tle2819@students.mu.ac.ke>
2023-07-18 11:25:14 +0100

    1

---
+++ Vanilla Js/minify.js
@@ -0,0 +1,3 @@
+// Minifying JavaScript and CSS:
+// Minifying involves removing unnecessary characters like comments, whitespace, and renaming variables to shorten code files' size without altering their functionality. This process can significantly reduce the download time for these files. There are various tools available online that can minify your JavaScript and CSS files automatically, or you can use build tools like Webpack or Gulp.
+
